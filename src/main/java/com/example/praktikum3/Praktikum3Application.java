15f1f35fae91b54057a962cab319b0bafc312614
darasdarmawan <darasyauqidarmawan@gmail.com>
2026-03-14 13:53:10 +0700

    Initial commit

---
+++ src/main/java/com/example/praktikum3/Praktikum3Application.java
@@ -0,0 +1,13 @@
+package com.example.praktikum3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Praktikum3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Praktikum3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/praktikum3/Praktikum3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praktikum3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praktikum3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
